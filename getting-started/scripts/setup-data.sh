4b8e2eb8c9aee40190b0b5848642f03ff6d7612d
John Cowie Del Corral <john@go-atomic.io>
2024-07-18 12:04:58 +0200

    Tidy up script

--- getting-started/scripts/setup-data.sh
+++ getting-started/scripts/setup-data.sh
@@ -18,23 +18,10 @@ fi
 
 SCRIPT_DIR=$(dirname "$(realpath "$0")")
 
-#SNOWFLAKE_CONNECTION_NAME=rai_getting_started
-#
-#export SNOWFLAKE_ACCOUNT=$SNOWFLAKE_ACCOUNT_NAME
-#export SNOWFLAKE_USER=$SNOWFLAKE_USERNAME
-#export SNOWFLAKE_PASSWORD=$SNOWFLAKE_PASSWORD
-
-#snow connection add \
-#--connection-name $SNOWFLAKE_CONNECTION_NAME \
-#--account "$SNOWFLAKE_ACCOUNT_NAME" \
-#--username "$SNOWFLAKE_USERNAME" \
-#--password "$SNOWFLAKE_PASSWORD"
-
 # run sql to setup create database called rai_getting_started and schema called power_transmission_network
 snow sql \
 -f "$SCRIPT_DIR/setup-schema.sql" \
 --temporary-connection
-
 
 # set up stage called gettingstartedcsvs
 snow stage create gettingstartedcsvs \
