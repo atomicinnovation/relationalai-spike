99358e659a539e472926f8a816412a8dc8e9bbf9
John Cowie Del Corral <john@go-atomic.io>
2024-07-26 16:28:50 +0200

    Add some notebooks

---
+++ getting-started/scripts/connect-data.sh
@@ -0,0 +1,5 @@
+rai imports:stream --source rai_getting_started.power_transmission_network.nodes --model power_transmission_network
+rai imports:stream --source rai_getting_started.power_transmission_network.powerlines --model power_transmission_network
+
+# Run the following to check status
+# rai imports:list --model power_transmission_network
